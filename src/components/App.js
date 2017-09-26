7a05f141377e49c33947a7eb9c1615d2886cfacf
Benjy <benjaminjosephevans1@gmail.com>
2017-09-26 13:01:06 +0100

    added sidenav

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import logo from '../logo.svg';
 import bgImage from '../background.svg';
-import '../App.css';
 import Landing from './Landing';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
+import '../App.css';
 import Header from './Header';
 import Twitter from './Twitter';
 import Footer from './Footer';
 import Showreel from './Showreel';
 import Gallery from './Gallery';
 import Contact from './Contact';
+import $ from 'jquery';
 
 class App extends Component {
+	componentDidMount(){
+		$(".button-collapse").sideNav();
+	  };
 	render() {
 		return (
 			<div style={background} className="blue">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,63 @@
-import React, { Component } from 'react';
-import nathan from '../nathan.jpg';
+import React, { Component } from "react";
+import nathan from "../nathan.jpg";
+
 class Header extends Component {
 	render() {
-		return (
-			<div>
-				<nav className="navbar-fixed transparent z-depth-1">
-					<div className="nav-wrapper container">
-						<a href="/" className="brand-logo">
-							<img src={nathan} className="circle responsive-img"/>
-						</a>
-						<ul className="right nav-content">
-							<li>
-								{' '}
-								<a href="/">
-									<i className="large material-icons">home</i>
-								</a>
-							</li>
-							<li>
-								<a href="/showreel">Showreel</a>
-							</li>
-							<li>
-								<a href="/gallery">Gallery</a>
-							</li>
-							<li>
-								<a href="/twitter">Social Media</a>
-							</li>
-							<li>
-								<a href="/contact">Contact</a>
-							</li>
-						</ul>
-					</div>
-				</nav>
-			</div>
-		);
-	}
+	  
+    return (
+      <div>
+        <nav className="navbar-fixed transparent z-depth-1">
+          <div className="nav-wrapper container">
+            <a href="/" className="brand-logo">
+              <img src={nathan} className="circle responsive-img App-logo" />
+            </a>
+            <ul className="right nav-content hide-on-med-and-down">
+              <li>
+                <a href="/">
+                  <i className="large material-icons">home</i>
+                </a>
+              </li>
+              <li>
+                <a href="/showreel">Showreel</a>
+              </li>
+              <li>
+                <a href="/gallery">Gallery</a>
+              </li>
+              <li>
+                <a href="/twitter">Social Media</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+			<ul id="slide-out" className="side-nav">
+            <li>
+                <a href="/">
+                  <i className="large material-icons">home</i>
+                </a>
+              </li>
+              <li>
+                <a href="/showreel">Showreel</a>
+              </li>
+              <li>
+                <a href="/gallery">Gallery</a>
+              </li>
+              <li>
+                <a href="/twitter">Social Media</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+            <a href="#" data-activates="slide-out" className="button-collapse">
+              <i className="material-icons">menu</i>
+            </a>
+          </div>
+        </nav>
+		
+      </div>
+    );
+  }
 }
 
 export default Header;
